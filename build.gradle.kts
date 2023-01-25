aecb291e34a64fae8e1b26c9c256aa1db045918f
fan87 <fan87.TW@gmail.com>
2023-01-25 20:42:19 +0800

    Added option to disable native building

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,38 +20,42 @@ dependencies {
 
 
 tasks {
+    if (System.getProperty("me.fan87.buildnative") == "true") {
 
-    register<Copy>("copyInjectorLib") {
-        group = "native"
-        dependsOn(":native-injector:build")
-        File(project(":native-injector").buildDir, "lib/main/debug/").listFiles()?.let { from(it.first()) }
-        into(sourceSets.main.get().resources.sourceDirectories.first().absolutePath)
-    }
-    register<Copy>("copyInjectionTarget") {
-        group = "native"
-        if (isWindows()) {
-            dependsOn(":injection-target-windows:build")
-            File(project(":injection-target-windows").buildDir, "lib/main/debug").listFiles()?.let { from(it.first()) }
-        } else {
-            dependsOn(":injection-target-linux:build")
-            File(project(":injection-target-linux").buildDir, "libs/").listFiles()?.let { from(it.first()) }
+        register<Copy>("copyInjectorLib") {
+            group = "native"
+            dependsOn(":native-injector:build")
+            File(project(":native-injector").buildDir, "lib/main/debug/").listFiles()?.let { from(it.first()) }
+            into(sourceSets.main.get().resources.sourceDirectories.first().absolutePath)
         }
-        into(sourceSets.main.get().resources.sourceDirectories.first().absolutePath)
+        register<Copy>("copyInjectionTarget") {
+            group = "native"
+            if (isWindows()) {
+                dependsOn(":injection-target-windows:build")
+                File(project(":injection-target-windows").buildDir, "lib/main/debug").listFiles()
+                    ?.let { from(it.first()) }
+            } else {
+                dependsOn(":injection-target-linux:build")
+                File(project(":injection-target-linux").buildDir, "libs/").listFiles()?.let { from(it.first()) }
+            }
+            into(sourceSets.main.get().resources.sourceDirectories.first().absolutePath)
 
-    }
+        }
 
-    register<Copy>("copyNativeAgent") {
-        group = "native"
-        dependsOn(":native-agent-native:build")
-        File(project(":native-agent-native").buildDir, "lib/main/debug").listFiles()?.filter { it.name.endsWith(".dll") || it.name.endsWith(".so") }?.let { from(it.first()) }
-        into(project("native-agent").sourceSets.main.get().resources.sourceDirectories.first().absolutePath)
+        register<Copy>("copyNativeAgent") {
+            group = "native"
+            dependsOn(":native-agent-native:build")
+            File(project(":native-agent-native").buildDir, "lib/main/debug").listFiles()
+                ?.filter { it.name.endsWith(".dll") || it.name.endsWith(".so") }?.let { from(it.first()) }
+            into(project("native-agent").sourceSets.main.get().resources.sourceDirectories.first().absolutePath)
 
-    }
-    register("copyAll") {
-        group = "native"
-        dependsOn("copyInjectorLib")
-        dependsOn("copyInjectionTarget")
-        dependsOn("copyNativeAgent")
+        }
+        register("copyAll") {
+            group = "native"
+            dependsOn("copyInjectorLib")
+            dependsOn("copyInjectionTarget")
+            dependsOn("copyNativeAgent")
+        }
     }
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,23 @@
 rootProject.name = "java-injector"
 
-fun isWindows(): Boolean = "Windows" in System.getProperty("os.name")
 
+
+
+
+fun isWindows(): Boolean = "Windows" in System.getProperty("os.name")
 
 include("test-program")
 include("test-inject-target")
 include("common")
 include("native-agent")
-include("native-agent-native")
-include("native-injector")
 
-if (isWindows()) {
-    include("injection-target-windows")
-} else {
-    include("injection-target-linux")
+if (System.getProperty("me.fan87.buildnative") == "true") {
+    include("native-agent-native")
+    include("native-injector")
+
+    if (isWindows()) {
+        include("injection-target-windows")
+    } else {
+        include("injection-target-linux")
+    }
 }
-
